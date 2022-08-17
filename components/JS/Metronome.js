51f533500bcf2138b57237cff1f11a1b28c288bb
lzl <875159954@qq.com>
2022-08-18 00:30:19 +0800

    handle double touch error

--- components/JS/Metronome.js
+++ components/JS/Metronome.js
@@ -1,7 +1,7 @@
 import css from "../UI/Metronome.module.scss";
 
 import { useRef, useState, useEffect } from "react";
-import SoundMaker from './SoundMaker'
+import SoundMaker from "./SoundMaker";
 
 function Metronome(props) {
   const [bpm, setBpm] = useState(60);
@@ -10,36 +10,39 @@ function Metronome(props) {
   const [controlId, setControlId] = useState(-1);
   const barsRef = useRef();
 
-  function togglePlay() {
+  function togglePlay(e) {
+    e.preventDefault();
     setTicking(!ticking);
   }
-  useEffect(function sideEffectofTicking() {
-    const speed = 60000 / bpm;
-    let counter = 0;
-    let id = -1;
-    if (ticking) {
-      id = setInterval(() => {
-        const bars = Array.from(barsRef.current.children);
-        for (let i = 0; i < bars.length; i++){
-          if(i==counter)  {
-            bars[i].classList.add(css.currentBar);
-            bars[i].style.setProperty('--sparkle-time', `${speed/1000}s`)
-            console.log(bars[i].style.getPropertyValue('--sparkle-time'))
-            
-            }
-          else bars[i].classList.remove(css.currentBar)
-        }
-        counter = (counter + 1) % beatsPerMeasure;
-      }, speed);
-    }
-    return () => {
-      clearInterval(id);
-    };
-  }, [ticking, bpm, beatsPerMeasure]);
+  useEffect(
+    function sideEffectofTicking() {
+      const speed = 60000 / bpm;
+      let counter = 0;
+      let id = -1;
+      if (ticking) {
+        id = setInterval(() => {
+          const bars = Array.from(barsRef.current.children);
+          for (let i = 0; i < bars.length; i++) {
+            if (i == counter) {
+              bars[i].classList.add(css.currentBar);
+              bars[i].style.setProperty("--sparkle-time", `${speed / 1000}s`);
+              console.log(bars[i].style.getPropertyValue("--sparkle-time"));
+            } else bars[i].classList.remove(css.currentBar);
+          }
+          counter = (counter + 1) % beatsPerMeasure;
+        }, speed);
+      }
+      return () => {
+        clearInterval(id);
+      };
+    },
+    [ticking, bpm, beatsPerMeasure]
+  );
   function changeBpm(e) {
     setBpm(Number(e.target.value));
   }
   function handleControl(e) {
+    clearInterval(controlId);
     const isDecrease = e.target.innerText == "-";
     let boundary = 240,
       step = 1;
@@ -50,71 +53,67 @@ function Metronome(props) {
       setBpm((pre) => (pre == boundary ? pre : pre + step));
     };
     fn();
-    speedUp(300, setControlId, fn);
+    speedUp(200, setControlId, fn);
   }
-  function stopChanging() {
+  function stopChanging(e) {
     clearInterval(controlId);
+    setBpm((pre) => pre);
   }
   function changeBeats() {
     setbeatsPerMeasure((pre) => {
       const next = (pre + 1) % 9;
       if (next == 0) return 1;
       return next;
-    })
+    });
   }
   return (
     <div className={css.container}>
       <div className={css.screen}>
-        <span className={css.depictor}>Allegro</span>  
+        <span className={css.depictor}>Allegro</span>
         <span>{bpm}</span>
-      <div className={css.bars} ref={barsRef}>
-          {Array(beatsPerMeasure).fill(1).map((e, i) => <div key={i} className={css.bar} ></div>)}
+        <div className={css.bars} ref={barsRef}>
+          {Array(beatsPerMeasure)
+            .fill(1)
+            .map((e, i) => (
+              <div key={i} className={css.bar}></div>
+            ))}
+        </div>
       </div>
-    </div>
       <div className={css.controls}>
         <button
           className={`${css.button} ${css.decrease}`}
-          onMouseDown={handleControl}
-          onMouseUp={stopChanging}
-          onMouseOut={stopChanging}
-
-          onTouchStart={handleControl}
-          onTouchCancel={stopChanging}
-          onTouchEnd={stopChanging}
-          onTouchMove={stopChanging}
+          onPointerDown={handleControl}
+          onPointerCancel={stopChanging}
+          onPointerUp={stopChanging}
+          onPointerOut={stopChanging}
         >
           -
         </button>
         <button onPointerUp={togglePlay} className={css.player}>
-        {ticking ? (
-          <div className={css.square}></div>
-        ) : (
-          <div className={css.play}></div>
-        )}
-      </button>
+          {ticking ? (
+            <div className={css.square}></div>
+          ) : (
+            <div className={css.play}></div>
+          )}
+        </button>
         <button
           className={`${css.button} ${css.increase}`}
-          onMouseDown={handleControl}
-          onMouseUp={stopChanging}
-          onMouseOut={stopChanging}
-
-          onTouchStart={handleControl}
-          onTouchCancel={stopChanging}
-          onTouchEnd={stopChanging}
-          onTouchMove={stopChanging}
-
+          onPointerDown={handleControl}
+          onPointerCancel={stopChanging}
+          onPointerUp={stopChanging}
+          onPointerOut={stopChanging}
         >
           +
         </button>
       </div>
- 
+
       <button onPointerUp={changeBeats}>{beatsPerMeasure}beats</button>
       <SoundMaker tempo={bpm} ticking={ticking} />
     </div>
   );
 }
 function speedUp(interval, setId, callback) {
-  interval = interval < 100 ? 100 : interval;
+  interval = interval < 20 ? 20 : interval;
   setId(
     setTimeout(() => {
       callback();
@@ -122,5 +121,10 @@ function speedUp(interval, setId, callback) {
     }, interval)
   );
 }
-
+function getSpeedWithinLimit(speed) {
+  return clamp(speed, 30, 240);
+}
+function clamp(value, minValue, maxValue) {
+  return Math.max(minValue, Math.min(maxValue, value));
+}
 export default Metronome;
