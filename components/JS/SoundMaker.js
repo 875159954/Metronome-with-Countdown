f11fe6c6e6b87adea810c665cf666ec8afe54e5a
lzl <875159954@qq.com>
2022-08-18 00:20:53 +0800

    add debounce for tempo change

--- components/JS/SoundMaker.js
+++ components/JS/SoundMaker.js
@@ -1,12 +1,23 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect, useRef,useCallback} from "react";
 
 function SoundMaker(props) {
-  const { tempo,ticking } = props;
+  const { tempo, ticking } = props;
+  const [state, setState] = useState(0);
+  const changeSpeedDebounced = useCallback(debounce_leading(changeSpeed, 300), []);
+  
+  let bufferSourceRef = useRef();
+  let audioRef = useRef();
+
   useEffect(() => {
-    if (!tempo||!ticking) return;
-    const audio = setup();
-    return () => { audio.close() }
-  }, [tempo,ticking])
+    if (!audioRef.current) setup();
+  }, []);
+  useEffect(() => {
+    if (!tempo  ||!ticking|| !audioRef.current||!bufferSourceRef.current) return;
+    changeSpeedDebounced(audioRef.current,bufferSourceRef.current,tempo);
+    return () => {
+      if (audioRef.current) audioRef.current.suspend();
+    };
+  }, [tempo, ticking]);
 
   function setup() {
     const audio = new (AudioContext || webkitAudioContext)();
@@ -16,19 +27,18 @@ function SoundMaker(props) {
       audio.sampleRate
     );
     const channel1 = buffer.getChannelData(0);
-    
     const phase = 0; //
     const amplifier = 1; //Sound amplifier
     const duration_frames = audio.sampleRate / 50; // Time slice
     const pitchE = 370; // measure in Hz
-    
+
     for (let i = 0; i < duration_frames; i++) {
       channel1[i] = Math.sin(phase) * amplifier;
       phase += (2 * Math.PI * pitchE) / audio.sampleRate;
       phase -= phase > 2 * Math.PI ? 2 * Math.PI : 0;
       amplifier -= 1 / duration_frames;
     }
-  
+
     const ocurrence = tempo / 60; // n = speed (measure by seconds) / 60s
     const frequency = 1 / ocurrence; // f = 1/n
 
@@ -36,21 +46,33 @@ function SoundMaker(props) {
     bufferSource.buffer = buffer;
     bufferSource.loop = true;
     bufferSource.loopEnd = frequency;
-
     bufferSource.connect(audio.destination);
     bufferSource.start(0);
 
-    return audio;
+    audioRef.current = audio;
+    bufferSourceRef.current = bufferSource;
   }
 
-  return <div>
-  </div>;
-}
-function getSpeedWithinLimit(speed) {
-  return clamp(speed, 30, 240);
-}
-function clamp(value, minValue, maxValue) {
-  return Math.max(minValue, Math.min(maxValue, value));
+  return <div>{`${tempo} ${ticking} ${state} `}</div>;
 }
 
+function changeSpeed(audio,source,tempo) {
+  audio.resume();
+  source.loopEnd = (1 / tempo) * 60;
+  console.log(audio,source);
+}
+
+function debounce_leading(func, timeout = 300){
+  let timer;
+  return (...args) => {
+    if (!timer) {
+      func.apply(this, args);
+    }
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = undefined;
+      func.apply(this, args);
+    }, timeout);
+  };
+}
 export default SoundMaker;
