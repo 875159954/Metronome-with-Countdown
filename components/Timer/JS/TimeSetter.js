e7ee990c593084e797ca642df021a8dd903f16d7
lzl <875159954@qq.com>
2022-08-20 16:12:10 +0800

    Visualizer demo done

--- components/Timer/JS/TimeSetter.js
+++ components/Timer/JS/TimeSetter.js
@@ -2,9 +2,11 @@ import css from "../UI/TimeSetter.module.scss";
 import React,{useEffect,useState,useContext} from "react";
 import { stateManager } from './Timer'
 import useLongPress from "../../../hooks/useLongPress";
+import { MyAudioContext } from "../../ContextProvider";
 function TimeSetter(props) {
   const { focusTime,setFocusTime } = props;
   const stateContext = useContext(stateManager)
+  const audioContext = useContext(MyAudioContext)
   const changeFoucusTime = useLongPress({down:setTime,up:stopChanging,interval:300})
   function stopChanging(e) {
   }
@@ -20,13 +22,13 @@ function TimeSetter(props) {
   function startFocus() {
     stateContext.nextState("START");
       // const context = new AudioContext();
-      // const osc = context.createOscillator();
-      // osc.connect(context.destination);
-
+      const osc = audioContext.audioContext.createOscillator();
+      osc.connect(audioContext.audioContext.destination);
+      
       // /* Let's play a sine wave for one second. */
 
-      // osc.start(context.currentTime+5);
-      // osc.stop(context.currentTime + 5.5);
+      osc.start(audioContext.audioContext.currentTime+5);
+      osc.stop(audioContext.audioContext.currentTime + 5.5);
   }
   return (
     <div className={css.container} >
